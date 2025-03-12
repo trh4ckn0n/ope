841b995c81df8ddf3050a42c959d4cd9e19dd8ff
trh4ckn0n <trhacknon@proton.me>
2025-03-12 02:34:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ elif page == "Opérations en cours":
     # Base de données des opérations Anonymous
     data = pd.DataFrame({
         "Ville": ["Paris", "New York", "Berlin", "Tokyo", "Roma", "Gaza", "Minsk", "Kiev"],
-        "Latitude": [48.8566, 40.7128, 52.5200, 35.6895, 41.9028, 31.5, 50.4501],
-        "Longitude": [2.3522, -74.0060, 13.4050, 139.6917, 12.4964, 34.47, 30.5234],
+        "Latitude": [48.8566, 40.7128, 52.5200, 35.6895, 41.9028, 31.5, 50.4501, 50.4501],
+        "Longitude": [2.3522, -74.0060, 13.4050, 139.6917, 12.4964, 34.47, 30.5234, 30.5234],
         "Opération": ["#OpFrance", "#OpUSA", "#OpGermany", "#OpJapan", "#OpItalia", "#OpIsrahell", "#OpRussia", "#OpFckPtn"]
     })
 
